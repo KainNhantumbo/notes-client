e286eef2ed401ce0fadff41eed63dc3a00375162
Kain Nhantumbo <nhantumbok@gmail.com>
2023-08-21 23:18:18 +0200

    feat: added initial theme colors.

---
+++ src/styles/themes.ts
@@ -0,0 +1,33 @@
+import type { Theme } from '../@types';
+
+export const light_default: Theme = {
+  error: '253, 56, 79',
+  font: '95, 99, 104',
+  font_dimmed: '95, 99, 104',
+  black: '12, 3, 13',
+  white: '255, 255, 255',
+  primary: '253, 200, 134',
+  secondary: '255, 71, 71',
+  background: '250, 250, 250',
+  foreground: '255, 255, 255',
+  primary_shade: '228, 113, 49',
+  secondary_shade: '157, 145, 255',
+  background_shade: '245, 242, 234',
+  foreground_shade: '240, 240, 240',
+};
+
+export const dark_default: Theme = {
+  error: '253, 56, 79',
+  font: '198, 197, 205',
+  font_dimmed: '25, 25, 28',
+  black: '25, 25, 28',
+  white: '240, 240,240',
+  primary: '253, 200, 134',
+  secondary: '255, 71, 71',
+  background: '32, 33, 36',
+  foreground: '41, 42, 46',
+  primary_shade: '228, 113, 49',
+  secondary_shade: '157, 145, 255',
+  background_shade: '32, 33, 36',
+  foreground_shade: '34, 37, 41',
+};
